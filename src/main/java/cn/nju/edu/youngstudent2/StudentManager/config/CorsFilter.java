b40770ffa4272679bc32bf3a3e1b6f6d42c0ef5f
youngstudent2 <644699280@qq.com>
2021-05-05 19:56:27 +0800

    跨域配置

---
+++ src/main/java/cn/nju/edu/youngstudent2/StudentManager/config/CorsFilter.java
@@ -0,0 +1,31 @@
+package cn.nju.edu.youngstudent2.StudentManager.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) arg0;
+        HttpServletResponse response = (HttpServletResponse) arg1;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, GET");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        arg2.doFilter(arg0, arg1);
+
+    }
+
+}
