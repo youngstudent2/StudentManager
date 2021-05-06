0641c8eb516174045482e980ebc4782cc316e71e
youngstudent2 <644699280@qq.com>
2021-05-06 22:32:10 +0800

    Update CorsFilter.java

--- src/main/java/cn/nju/edu/youngstudent2/StudentManager/config/CorsFilter.java
+++ src/main/java/cn/nju/edu/youngstudent2/StudentManager/config/CorsFilter.java
@@ -12,9 +12,6 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
 @WebFilter(filterName = "CorsFilter")
 public class CorsFilter implements Filter {
 
